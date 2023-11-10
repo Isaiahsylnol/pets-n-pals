d3975cef76d377e2fd13a6f8fa9e3c5a18020089
Isaiahsylnol <pictureisaiah@gmail.com>
2023-11-10 14:34:14 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [
