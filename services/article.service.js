6309348a173e8cad415cc2545ed222e1d07b3ad6
Isaiahsylnol <pictureisaiah@gmail.com>
2024-05-27 18:54:42 -0400

    Article Service code cleanup

--- services/article.service.js
+++ services/article.service.js
@@ -1,16 +1,11 @@
 import axios from "axios";
 
-// const fetchArticles = () => {
-//   return axios.get(`${process.env.NEXT_PUBLIC_BACKEND}/api/article/all`);
-// };
-
 const fetchArticlesByBreed = (breeds) => {
   return axios.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/article/all`, {
     breed: breeds,
   });
 };
 const ArticleService = {
-  // fetchArticles,
   fetchArticlesByBreed,
 };
 
